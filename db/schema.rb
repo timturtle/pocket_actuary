3e3bf1d22b13279701596ef0036ebc8ef6e06b8a
trippt02 <timtripp@outlook.com>
2021-10-01 10:00:08 -0400

    added stuff

---
+++ db/schema.rb
@@ -0,0 +1,14 @@
+
+ActiveRecord::Schema.define(version: 2021_09_30_223736) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
